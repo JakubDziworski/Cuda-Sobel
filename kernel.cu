0006a4d98071fa1ac1d11150201ad9ba9812e3e9
jakubdziworski <jakub.dziworski@gmail.com>
2015-06-16 21:17:04 +0200

    old

--- kernel.cu
+++ kernel.cu
@@ -23,34 +23,6 @@ __device__ int sobel(int a, int b, int c, int d, int e, int f) {
 	return ((a + 2 * b + c) - (d + 2 * e + f));
 }
 
-__device__ unsigned char
-ComputeSobel(unsigned char ul, // upper left
-unsigned char um, // upper middle
-unsigned char ur, // upper right
-unsigned char ml, // middle left
-unsigned char mm, // middle (unused)
-unsigned char mr, // middle right
-unsigned char ll, // lower left
-unsigned char lm, // lower middle
-unsigned char lr, // lower right
-float fScale)
-{
-	short Horz = ur + 2 * mr + lr - ul - 2 * ml - ll;
-	short Vert = ul + 2 * um + ur - ll - 2 * lm - lr;
-	short Sum = (short)(fScale*(abs((int)Horz) + abs((int)Vert)));
-
-	if (Sum < 125)
-	{
-		return 0;
-	}
-	else if (Sum > 125)
-	{
-		return 255;
-	}
-
-	return (unsigned char)Sum;
-}
-
 __device__ int getindexForPixelAt(unsigned char *pixels, int x, int y, int width, int height)
 {
 	int val = 4 * (x + y*width);
@@ -83,25 +55,52 @@ __global__ void addKernel(unsigned char *pixels,int width,int height)
 	int y = threadIdx.y + blockIdx.y * blockDim.y;
 
 	if (y >= height-1 || x >= width-1) return;
-		float4 ul, um, ur, ml, mm, mr, ll, lm, lr;
+		float4 x0, x1, x2, x3, x4, x5, x6, x7, x8;
+		x0 = pixelAt(pixels, x - 1, y - 1, width,height);
+		x1 = pixelAt(pixels, x, y - 1, width, height);
+		x2 = pixelAt(pixels, x + 1, y, width, height);
+		x3 = pixelAt(pixels, x - 1, y, width, height);
+		x4 = pixelAt(pixels, x, y, width, height);
+		x5 = pixelAt(pixels, x + 1, y, width, height);
+		x6 = pixelAt(pixels, x - 1, y + 1, width, height);
+		x7 = pixelAt(pixels, x, y + 1, width, height);
+		x8 = pixelAt(pixels, x + 1, y + 1, width, height);
 
-		ul = pixelAt(pixels, x - 1, y + 1, width, height);
-		um = pixelAt(pixels, x, y + 1, width, height);
-		ur = pixelAt(pixels, x + 1, y + 1, width, height);
+		int dfdy_r = sobel(x6.x, x7.x, x8.x, x0.x, x1.x, x2.x);
+		int dfdx_r = sobel(x2.x, x5.x, x8.x, x0.x, x3.x, x6.x);
 
-		ml = pixelAt(pixels, x - 1, y, width, height);
-		mm = pixelAt(pixels, x, y, width, height);
-		mr = pixelAt(pixels, x+1, y, width, height);
+		int dfdy_g = sobel(x6.y, x7.y, x8.y, x0.y, x1.y, x2.y);
+		int dfdx_g = sobel(x2.y, x5.y, x8.y, x0.y, x3.y, x6.y);
 
-		ll = pixelAt(pixels, x -1, y - 1, width, height);
-		lm = pixelAt(pixels, x, y - 1, width, height);
-		lr = pixelAt(pixels, x+1, y - 1, width, height);
+		int dfdy_b = sobel(x6.z, x7.z, x8.z, x0.z, x1.z, x2.z);
+		int dfdx_b = sobel(x2.z, x5.z, x8.z, x0.z, x3.z, x6.z);
 
+		int gradient_r = abs(dfdy_r) + abs(dfdx_r);
+		int gradient_g = abs(dfdy_g) + abs(dfdx_g);
+		int gradient_b = abs(dfdy_b) + abs(dfdx_b);
+
+		float mean_gradient = (gradient_r + gradient_g + gradient_b) / 3.0f;
+		unsigned char edge = (mean_gradient > PRIMARY_THRESHOLD);
+		unsigned char slight_edge = (mean_gradient > SECONDARY_THRESHOLD);
+
+		float4 new_pixel = float4{ 0, 0, 0, 255 };
+
+		new_pixel.x = 255 * edge | 125 * slight_edge;
+		new_pixel.y = 255 * edge | 125 * slight_edge;
+		new_pixel.z = 255 * edge | 125 * slight_edge;
+
+		
 		int pos = getindexForPixelAt(pixels, x, y, width, height);
-		pixels[pos] = ComputeSobel(ul.x, um.x, ur.x, ml.x, mm.x, mr.x, ll.x, lm.x, lr.x, 1.0f);
-		pixels[pos+1] = ComputeSobel(ul.y, um.y, ur.y, ml.y, mm.y, mr.y, ll.y, lm.y, lr.y, 1.0f);
-		pixels[pos+2] = ComputeSobel(ul.z, um.z, ur.z, ml.z, mm.z, mr.z, ll.z, lm.z, lr.z, 1.0f);
-		pixels[pos + 3] = ComputeSobel(ul.w, um.w, ur.w, ml.w, mm.w, mr.w, ll.w, lm.w, lr.w, 1.0f);
+
+
+		/*if (pixels[pos] == NULL || pixels[pos + 1] == NULL || pixels[pos + 2] == NULL) {
+			printf("ERROR pos = %d, x = %d %y = %d");
+		}*/
+
+
+		pixels[pos] = new_pixel.x;
+		pixels[pos + 1] = new_pixel.y;
+		pixels[pos + 2] = new_pixel.z;
 }
 
 extern "C" void initFilter(unsigned char *pixels, int width, int height)
